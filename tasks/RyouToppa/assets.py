cadb10b96b969694775314056cbbdba3a74aa155
necro-wbj <a163.2619699@gmail.com>
2024-03-28 18:58:08 +0800

    修正非寮管理，且寮未開導致出錯的問題

--- tasks/RyouToppa/assets.py
+++ tasks/RyouToppa/assets.py
@@ -132,6 +132,8 @@ class RyouToppaAssets:
 	I_REAL_RAID_REFRESH = RuleImage(roi_front=(963,569,174,60), roi_back=(963,569,174,60), threshold=0.8, method="Template matching", file="./tasks/RyouToppa/res/res_real_raid_refresh.png")
 	# 击破后的寮奖励 
 	I_RYOU_REWARD_90 = RuleImage(roi_front=(134,415,232,38), roi_back=(134,415,232,38), threshold=0.8, method="Template matching", file="./tasks/RyouToppa/res/res_ryou_reward_90.png")
+	# description 
+	I_WAIT_START_RYOU_TOPPA = RuleImage(roi_front=(460,583,357,40.33333333333326), roi_back=(460,583,357,40.33333333333326), threshold=0.8, method="Template matching", file="./tasks/RyouToppa/res/res_wait_start_ryou_toppa.png")
 
 
 	# Ocr Rule Assets
