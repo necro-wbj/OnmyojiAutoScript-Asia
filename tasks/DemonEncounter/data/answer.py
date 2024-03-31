13286061b8262455e3b74d4180fbab5bbc25f05b
tingo5og <tingo5og@gmail.com>
2024-03-31 22:36:39 +0800

    重寫 逢魔密信的判斷 找相似 而不是全一樣
    TODO 如果答錯他會一直案同樣的答案 這個要改 改成TINE OUT?

--- tasks/DemonEncounter/data/answer.py
+++ tasks/DemonEncounter/data/answer.py
@@ -5,6 +5,7 @@ import csv
 
 from datetime import datetime
 from pathlib import Path
+from module.logger import logger
 
 
 def answer_one(question: str, options: list[str]) -> int:
@@ -15,17 +16,57 @@ def answer_one(question: str, options: list[str]) -> int:
     :param options:
     :return:
     """
+    # file = str(Path(__file__).parent / 'data.csv')
+    # with open(file, newline='', encoding='utf-8-sig') as csvfile:
+    #     reader = csv.reader(csvfile)
+    #     next(reader)
+    #     for row in reader:
+    #         if row[0] == question:
+    #             try:
+    #                 return options.index(row[1]) + 1
+    #             except ValueError:
+    #                 return 1
+    #     return 1
+    ## 我要重寫上面的代碼 改為找最相似的 而不是完全一樣的
+    question_lcut = set(list(str(question)))
     file = str(Path(__file__).parent / 'data.csv')
     with open(file, newline='', encoding='utf-8-sig') as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
+        max_score = 0
+        max_score_index = 0
         for row in reader:
-            if row[0] == question:
-                try:
-                    return options.index(row[1]) + 1
-                except ValueError:
-                    return 1
-        return 1
+            # score 為row[0]與question的相似度
+            score = 0
+            question_row_lcut = set(list(str(row[0])))
+            score = len(question_lcut & question_row_lcut) / len(question_lcut | question_row_lcut)
+            if score > max_score:
+                max_score = score
+                max_score_index = row[1]
+        # here match the answer
+        Ans_lcut = set(list(str(row[1])))
+        Ans_score = 0
+        Ans_score_max = 0
+        final_ans = 0
+        # check fully match answer exists or use similar answer
+        if row[1] in options:
+            return options.index(row[1]) + 1
+        else:
+            for option in options:
+                option_lcut = set(list(str(option)))
+                Ans_score = len(Ans_lcut & option_lcut) / len(Ans_lcut | option_lcut)
+                if Ans_score > Ans_score_max:
+                    Ans_score_max = Ans_score
+                    final_ans = option
+        try:
+            #print question and answer
+            print(question, final_ans)
+            return options.index(final_ans) + 1
+        except ValueError:
+            return 1
+    return 1
+
+
 
 
 if __name__ == "__main__":
