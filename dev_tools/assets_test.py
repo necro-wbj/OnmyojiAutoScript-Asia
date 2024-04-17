652000459b74b986cd670f9111189d5f9466bcb8
necro-wbj <a163.2619699@gmail.com>
2024-04-18 02:45:50 +0800

    修正呱太突破出現時進入寮突破失敗 & 修正dev_tools失效

--- dev_tools/assets_test.py
+++ dev_tools/assets_test.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('D:/onmyoji/OnmyojiAutoScript-easy-install')
 import cv2
 import numpy as np
 
@@ -36,10 +34,10 @@ def detect_ocr(file: str, target: RuleOcr):
 
 
 # 图片文件路径 可以是相对路径
-IMAGE_FILE = r'test.png'
+IMAGE_FILE = r'./dev_tools/test.png'
 if __name__ == '__main__':
-    from tasks.KekkaiActivation.assets import KekkaiActivationAssets
-    targe = KekkaiActivationAssets.I_A_HARVEST_FISH_6
+    from tasks.RyouToppa.assets import RyouToppaAssets
+    targe = RyouToppaAssets.I_RYOU_TOPPA
     print(detect_image(IMAGE_FILE, targe))
     # from tasks.Restart.assets import RestartAssets
     # target = RestartAssets.O_LOGIN_ENTER_GAME
