06a666c493443cde9bcb0e6815ffacabe5852b04
necro-wbj <a163.2619699@gmail.com>
2024-05-16 01:26:15 +0800

    修正邀請房間判斷失效的問題

--- tasks/Component/GeneralInvite/assets.py
+++ tasks/Component/GeneralInvite/assets.py
@@ -84,12 +84,12 @@ class GeneralInviteAssets:
 	# 所有的组队界面都有加成 
 	I_GI_BUFF = RuleImage(roi_front=(794,38,46,42), roi_back=(794,38,46,42), threshold=0.8, method="Template matching", file="./tasks/Component/GeneralInvite/gi/gi_gi_buff.png")
 	# 三人御魂组队左上角的协站队伍 
-	I_GI_IN_ROOM = RuleImage(roi_front=(92,17,213,64), roi_back=(92,17,213,64), threshold=0.8, method="Template matching", file="./tasks/Component/GeneralInvite/gi/gi_in_room.png")
+	I_GI_IN_ROOM = RuleImage(roi_front=(92,17,162,46), roi_back=(92,17,162,46), threshold=0.8, method="Template matching", file="./tasks/Component/GeneralInvite/gi/gi_gi_in_room.png")
 
 
 	# Image Rule Assets
 	# description 
-	I_GI_EMOJI_1 = RuleImage(roi_front=(29,538,49,54), roi_back=(17,527,74,75), threshold=0.8, method="Template matching", file="./tasks/Component/GeneralInvite/gi/gi_gi_emoji_1.png")
+	I_GI_EMOJI_1 = RuleImage(roi_front=(30,531,46,45), roi_back=(12,512,83,94), threshold=0.8, method="Template matching", file="./tasks/Component/GeneralInvite/gi/gi_gi_emoji_1.png")
 	# description 
 	I_GI_EMOJI_2 = RuleImage(roi_front=(27,622,55,51), roi_back=(27,622,55,51), threshold=0.8, method="Template matching", file="./tasks/Component/GeneralInvite/gi/gi_gi_emoji_2.png")
 	# 判断是不是在庭院界面 
