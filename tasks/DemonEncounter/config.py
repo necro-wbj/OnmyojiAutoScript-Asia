5fc9ea6cb7f88a4ebc6cf143ae44bdd537d16c93
tingo5og <tingo5og@gmail.com>
2024-06-29 00:43:52 +0800

    支援 極逢魔BOSS 逢魔隊伍
    1.Add 極逢魔的按鈕
    2.Add 逢魔的隊伍
    3.逢魔密信重複問題答案合併

--- tasks/DemonEncounter/config.py
+++ tasks/DemonEncounter/config.py
@@ -9,12 +9,18 @@ from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase, TimeDelta
 from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
 from tasks.Utils.config_enum import ShikigamiClass
+from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
 
 class UtilizeScheduler(Scheduler):
     priority = Field(default=2, description='priority_help')
 
+class SuperBossConfig(BaseModel):
+    find_super_boss: bool = Field(default=False, description='FIGHT SUPER BOSS')
+
 class DemonEncounter(ConfigBase):
     scheduler: UtilizeScheduler = Field(default_factory=UtilizeScheduler)
+    switch_soul: SwitchSoulConfig = Field(default_factory=SwitchSoulConfig)
+    super_boss_config: SuperBossConfig = Field(default_factory=SuperBossConfig)
     # general_battle_config: GeneralBattleConfig = Field(default_factory=GeneralBattleConfig)
 
 
