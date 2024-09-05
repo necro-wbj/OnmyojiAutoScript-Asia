97cf8991449d52e8c0b3f8d91e0827f829e02d1f
tingo5og <tingo5og@gmail.com>
2024-09-06 02:57:17 +0800

    improve login
    1.add contract agree
    2.add comment request

--- tasks/Restart/assets.py
+++ tasks/Restart/assets.py
@@ -62,6 +62,10 @@ class RestartAssets:
 	I_LOGIN_FIX = RuleImage(roi_front=(1196,514,60,54), roi_back=(1196,514,60,54), threshold=0.8, method="Template matching", file="./tasks/Restart/login/login_login_fix.png")
 	# description 
 	I_LOGIN_DOWNLOAD_DRAW = RuleImage(roi_front=(743,459,100,42), roi_back=(651,328,320,241), threshold=0.8, method="Template matching", file="./tasks/Restart/login/login_login_download_draw.png")
+	# 同意协议 
+	I_LOGIN_CONTRACT_AGREE = RuleImage(roi_front=(535,575,209,62), roi_back=(535,575,209,62), threshold=0.8, method="Template matching", file="./tasks/Restart/login/login_login_contract_agree.png")
+	# 请求评论 
+	I_LOGIN_COMMENT_REQUEST = RuleImage(roi_front=(535,523,201,42), roi_back=(535,523,201,42), threshold=0.8, method="Template matching", file="./tasks/Restart/login/login_login_comment_request.png")
 
 
 	# Ocr Rule Assets
