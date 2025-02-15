825a1ced8fcc134d3878e72bf1356efae6b9cdaa
tingo5og <tingo5og@gmail.com>
2025-02-15 22:14:49 +0800

    update "經驗妖怪", "金幣妖怪"

--- tasks/ExperienceYoukai/config.py
+++ tasks/ExperienceYoukai/config.py
@@ -5,7 +5,14 @@ from pydantic import BaseModel, Field
 
 from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase
+from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
+
+class ExpYoukaiConfig(ConfigBase):
+    buff_exp_50_click: bool = Field(default=False)
+    buff_exp_100_click: bool = Field(default=False)
 
 class ExperienceYoukai(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
+    exp_youkai: ExpYoukaiConfig = Field(default_factory=ExpYoukaiConfig)
+    switch_soul: SwitchSoulConfig = Field(default_factory=SwitchSoulConfig)
 
