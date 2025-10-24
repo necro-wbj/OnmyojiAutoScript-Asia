6ba0d7c6c17d7abae2989a144e0991fa832c5c92
tingo5og <tingo5og@gmail.com>
2025-10-24 21:39:27 +0800

    fix KekkaiActivation
    註解掉因為無法區分黃色/灰色的啟動 而造成的無窮LOOP
    I_A_ACTIVATE_YELLOW/I_A_ACTIVATE_GRAY

--- tasks/KekkaiActivation/script_task.py
+++ tasks/KekkaiActivation/script_task.py
@@ -102,15 +102,15 @@ class ScriptTask(KU, KekkaiActivationAssets):
             card_effect = self.check_card_effect()
 
             # 不稳定太，等待动画结束
-            if not card_status and not card_effect:
-                # 黄色的 ”激活“
-                if self.appear(self.I_A_ACTIVATE_YELLOW, threshold=0.95):
-                    continue
-                if self.appear(self.I_A_DEMOUNT):
-                    # 现在在动画里面
-                    logger.info('Now in the animation')
-                    logger.info('Now there is no card')
-                    continue
+            # if not card_status and not card_effect:
+            #     # 黄色的 ”激活“
+            #     if self.appear(self.I_A_ACTIVATE_YELLOW, threshold=0.95):
+            #         continue
+            #     if self.appear(self.I_A_DEMOUNT):
+            #         # 现在在动画里面
+            #         logger.info('Now in the animation')
+            #         logger.info('Now there is no card')
+            #         continue
             # 如果这张卡生效着，在使用中
             if card_status and card_effect:
                 logger.info('Card is using')
